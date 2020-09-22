f0c9d55231b5fc44b6bd4ae787743185cf7c248a
Michelle Lin <michelleamazinglin@gmail.com>
2020-09-21 22:48:56 -0400

    finished hw ' '

--- W4D1/mancala/lib/board.rb
+++ W4D1/mancala/lib/board.rb
@@ -32,10 +32,32 @@ class Board
   end
 
   def make_move(start_pos, current_player_name)
+
+    stones = @board[start_pos]
+    @board[start_pos] = []
+
+    cup_idx = start_pos
+    until stones.empty?
+      cup_idx += 1
+      cup_idx = 0 if cup_idx > 13
+      if cup_idx == 6
+        @cups[6] << stones.pop if current_player_name == @name1
+      elsif cup_idx == 13
+        @cups[13] << stones.pop if current_player_name == @name2
+      else
+        @cups[cup_idx] << stones.pop
+      end
+    end
   end
 
   def next_turn(ending_cup_idx)
     # helper method to determine whether #make_move returns :switch, :prompt, or ending_cup_idx
+        if ending_cup_idx == 6 || ending_cup_idx == 13
+        elsif @cups[ending_cup_idx].count == 1
+        else
+          ending_cup_id
+        end
+
   end
 
   def render
@@ -47,8 +69,17 @@ class Board
   end
 
   def one_side_empty?
+    @cups.take(6).all? { |cup| cup.empty? } ||
+    @cups[7..12].all? { |cup| cup.empty? }
   end
 
   def winner
+    player1_count = @cups[6].count
+    player2_count = @cups[13].count
+
+    if player1_count == player2_count
+      :draw
+    else
+      player1_count > player2_count ? @name1 : @name2
   end
 end
